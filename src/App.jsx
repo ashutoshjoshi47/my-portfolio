5817a7e9794a18cf6f113a1b734ff581cc03b8ea
Aashutosh Joshi <125844407+ashutoshjoshi47@users.noreply.github.com>
2026-04-10 13:30:27 +0545

    Skill section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Portfolio from "./components/Portfolio"
@@ -15,6 +16,7 @@ function App() {
     <About />
     <Portfolio />
     <Skills />
+    <Contact />
 
     <SocialLinks />
    </div>
